c142cbaa4916f10554a882abeaa4dca5e958eb56
Jae Hoon Yang <didwogns789@naver.com>
2020-09-15 12:02:49 +0900

    Clear bonus task 5 version 2

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 
-import queryString from "query-string";
+// import queryString from "query-string";
 
 class Course extends Component {
   render() {
-    const parsed = queryString.parse(this.props.location.search);
-    console.log(this.props.location.search);
-    console.log(parsed.title);
-    const title = parsed.title;
+    // const parsed = queryString.parse(this.props.location.search);
+    // console.log(this.props.location.search);
+    // console.log(parsed.title);
+    // const title = parsed.title;
+    const search = this.props.location.search;
+    const params = new URLSearchParams(search);
+    const title = params.get("title");
 
     return (
       <div>
